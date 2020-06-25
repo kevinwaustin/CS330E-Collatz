33edc9ed8a902588a04015149b5e50f282dba3ce
“Kevin <kevinw.austin@utexas.edu>
2020-06-25 13:54:43 -0500

    updated test_eval cases with correct expected values and additional edge case tests

--- TestCollatz.py
+++ TestCollatz.py
@@ -57,19 +57,31 @@ class TestCollatz (TestCase):
 
     def test_eval_1(self):
         v = collatz_eval(1, 10)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 20)
 
     def test_eval_2(self):
         v = collatz_eval(100, 200)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 125)
 
     def test_eval_3(self):
         v = collatz_eval(201, 210)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 89)
 
     def test_eval_4(self):
         v = collatz_eval(900, 1000)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 174)
+    
+    def test_eval_5(self):
+        v = collatz_eval(10, 1)
+        self.assertEqual(v, 20)
+    
+    def test_eval_6(self):
+        v = collatz_eval(-1, 10)
+        self.assertEqual(v, 0)
+    
+    def test_eval_7(self):
+        v = collatz_eval(10, 10)
+        self.assertEqual(v, 7)
 
     # -----
     # print
